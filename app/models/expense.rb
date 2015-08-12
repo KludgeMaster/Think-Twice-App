7a647aa38403bb63466899f14a15b68b966648b3
Daniel Sanudo <alejandro-sanudo@hotmail.com>
2015-08-12 16:06:49 -0700

    Gave users and expenses validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ActiveRecord::Base
+  validates :name, presence: true
+  validates :value, numericality: {only_integer: true}
+  validates :value, numericality: {greater_than: 0}
   
 end
