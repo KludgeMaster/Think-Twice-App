4e1d8a1fe8928562b3ff6cbf1ca8c0efb5a657a5
KludgeMaster <yiryans@gmai.com>
2015-08-12 23:40:54 +0000

    ryan's part

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,42 @@
 # Homepage (Root path)
+helpers do
+  def current_user
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
+  def all_songs
+    @songs = Song.all
+  end
+end
+
 get '/' do
   erb :index
+end
+
+get '/expenses' do
+  @expenses = Expense.find_by(user: current_user)
+  erb :expenses
+end
+
+post '/input' do
+  if input.is_i? && input > 0
+    redirect '/result'
+  else
+    redirect '/'
+  end
+end
+
+
+post 'expenses/add' do
+  @expense = Expenses.new(
+    name: params[:name],
+    value: params[:value],
+    interval: params[:interval]
+  )
+  @expense.save
+  redirect '/expenses'
+end
+
+post 'expenses/delete/:id' do
+  Expenses.find(:id).destroy
+  redirect '/expenses'
 end
