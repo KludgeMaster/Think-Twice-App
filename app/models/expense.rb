25330cf580d3974ef08f9130d3cb78babf607816
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-13 18:41:44 +0000

    reduce lines in models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,5 @@
 class Expense < ActiveRecord::Base
   validates :name, presence: true
-  validates :value, numericality: {only_integer: true}
-  validates :value, numericality: {greater_than: 0}
+  validates :value, numericality: {only_integer: true, greater_than: 0}
   
 end
