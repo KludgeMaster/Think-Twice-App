86129e25e528eb4815ed70a93cdf47a8fd78ee1e
Daniel Sanudo <alejandro-sanudo@hotmail.com>
2015-08-13 12:21:11 -0700

    reorganized actions

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,5 +2,4 @@ class Expense < ActiveRecord::Base
   validates :name, presence: true
   validates :value, numericality: {only_integer: true}
   validates :value, numericality: {greater_than: 0}
-  
 end
--- app/views/user/results.rb
+++ app/views/user/results.rb
@@ -1,36 +1,8 @@
 require 'pry'
-# interval_hash = {
-#   annually: 365,
-#   monthly: 30,
-#   biweekly: 14,
-#   weekly: 7,
-#   daily: 1,
-#   once: 1
-# }
-
-# tt_value = 5.00
-# expense_value = 1200.00
-# interval = "annually"
-# MINIMUM_VIABLE_PERCENT = 20
-
-
-# result = (tt_value * 100) / expense_value
-
-# if result < MINIMUM_VIABLE_PERCENT
-#   interval_hash.values.each do |interv|
-#     result = (tt_value * 100) / (expense_value * (interv.to_f / 365))
-#     # interval = 
-#     binding.pry
-#     break if result > MINIMUM_VIABLE_PERCENT
-#   end
-# end
-# puts result.round(2)
-
-#method returns [result, interval, times]
 MIN_PERCENT = 5
 
 tt = 50.00
-value = 10.00
+value = 10.to_f
 interval = 'monthly'
 
 result = tt*100/value
