694f9abd74c70bd013e84ee97e879e8aa5f19614
Daniel Sanudo <alejandro-sanudo@hotmail.com>
2015-08-14 12:00:35 -0700

    Moved TT algorithm to helpers method

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,58 @@ helpers do
   def current_user
     @user = User.find(session[:user_id]) if session[:user_id]
   end
+
+  def get_tt_values(expense_tt, expense_value, expense_interval)
+    tt = expense_tt.to_f
+    value = expense_value.to_f
+    interval = expense_interval.downcase
+    result = 0
+    times = nil
+    temp = {
+      annual: 365,
+      monthly: 30,
+      biweekly: 14,
+      weekly: 7,
+      daily: 1,
+      once: 1
+    }
+    daily_value = tt/(value/temp[interval.to_sym])
+    values = {
+      annual: (daily_value/365)*100,
+      monthly: (daily_value/30)*100,
+      biweekly: (daily_value/14)*100,
+      weekly: (daily_value/7)*100,
+      daily: (daily_value/1)*100,
+      # once: (daily_value/1)*100
+    }
+    values.each do |interv, percentage|
+      if percentage >= 40 && percentage <= 60
+        result = percentage
+        interval = interv
+        break
+      elsif percentage >= 25 && percentage < 40
+        result = percentage
+        interval = interv
+      elsif percentage <25 && percentage > 10  
+        result = percentage
+        interval = interv
+      elsif percentage >= 200 && percentage <= 300 
+        result = percentage
+        interval = interv
+      elsif percentage < 100
+        result = percentage
+        interval = interv
+      else
+        result = percentage
+        interval = interv
+      end
+    end  
+    if expense_interval == 'Once'
+      interval = ""
+      result = (tt * 100) / expense_value.to_f
+    end  
+    [result.round(1),interval,times]
+  end
 end
 
 get '/', '/index', '/home','/main' do
@@ -45,7 +97,7 @@ end
 post '/think-twice/' do
   @expenses = Expense.where(user_id: current_user.id) if current_user
   if params[:amount].to_i > 0
-    @tt = params[:amount]
+    @tt = params[:amount].to_f
     erb :'/results'
   else
     redirect '/'
