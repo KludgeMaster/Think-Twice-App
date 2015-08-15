0f4ecba3a1cd02923c1be7eb6f92716348c46bce
KludgeMaster <yiryans@gmai.com>
2015-08-15 23:50:00 +0000

    stat page added in when logged in as admin, blank sponsor page

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,9 +4,4 @@ class Expense < ActiveRecord::Base
   validates :value, numericality: {greater_than: 0, less_than: 10000000}
   
   belongs_to :user
-
-<<<<<<< HEAD
-=======
- 
->>>>>>> 2e36aef07e09341f7e623adda57104216554867f
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,25 @@
 User.destroy_all
 User.create!(name: "Ryan", email: "yiryans@gmail.com", password: "123")
 User.create!(name: "Alex", email: "Alex@gmail.com", password: "123")
+User.create!(name: "TTadmin", email: "ttadmin@gmail.com", password: "123", admin: true)
 
 Expense.destroy_all
-Expense.create!(user_id: 0, name: "Groceries", value: 200, interval: "biweekly")
-Expense.create!(user_id: 0, name: "Coffee", value: 25, interval: "weekly")
-Expense.create!(user_id: 0, name: "Transportation", value: 6, interval: "daily")
-Expense.create!(user_id: 0, name: "Car", value: 30000, interval: "once")
+Expense.create!(user_id: 0, name: "Groceries", value: 200, interval: "biweekly", category: "Food")
+Expense.create!(user_id: 0, name: "Coffee", value: 25, interval: "weekly", category:"Food")
+Expense.create!(user_id: 0, name: "Transportation", value: 6, interval: "daily", category: "Transportation")
+Expense.create!(user_id: 0, name: "Car", value: 30000, interval: "once", category:"Miscellaneous")
 
 
-Expense.create!(user_id: 1, name: "Rent", value: 1500, interval: "monthly")
-Expense.create!(user_id: 1, name: "Bus Pass", value: 125, interval: "monthly")
-Expense.create!(user_id: 1, name: "Headphone", value: 350, interval: "once")
-Expense.create!(user_id: 1, name: "Beer", value: 40, interval: "weekly")
-Expense.create!(user_id: 1, name: "Phone Bill", value: 100, interval: "monthly")
+Expense.create!(user_id: User.find_by(name: "Ryan").id, name: "Rent", value: 1500, interval: "monthly", category: "Accommodation")
+Expense.create!(user_id: User.find_by(name: "Ryan").id, name: "Bus Pass", value: 125, interval: "monthly",category:"Transportation")
+Expense.create!(user_id: User.find_by(name: "Ryan").id, name: "Headphone", value: 350, interval: "once", category: "Miscellaneous")
+Expense.create!(user_id: User.find_by(name: "Ryan").id, name: "Beer", value: 40, interval: "weekly", category: "Entertainmnet")
+Expense.create!(user_id: User.find_by(name: "Ryan").id, name: "Phone Bill", value: 100, interval: "monthly", category: "Utilities")
 
 
-Expense.create!(user_id: 2, name: "Rent", value: 1358, interval: "monthly")
-Expense.create!(user_id: 2, name: "Bus Pass", value: 108, interval: "monthly")
-Expense.create!(user_id: 2, name: "Internet Bill", value: 54, interval: "monthly")
-Expense.create!(user_id: 2, name: "Restaurant", value: 55, interval: "weekly")
-Expense.create!(user_id: 2, name: "Phone Bill", value: 97, interval: "monthly")
+Expense.create!(user_id: User.find_by(name: "TTadmin").id, name: "Rent", value: 1358, interval: "monthly", category: "Accommodation")
+Expense.create!(user_id: User.find_by(name: "TTadmin").id, name: "Bus Pass", value: 108, interval: "monthly", category: "Transportation")
+Expense.create!(user_id: User.find_by(name: "TTadmin").id, name: "Internet Bill", value: 54, interval: "monthly", category: "Utilities")
+Expense.create!(user_id: User.find_by(name: "TTadmin").id, name: "Restaurant", value: 17, interval: "once", category: "Food")
+Expense.create!(user_id: User.find_by(name: "TTadmin").id, name: "Phone Bill", value: 97, interval: "monthly", category:"Utilities")
 
